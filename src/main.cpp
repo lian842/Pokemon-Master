47ad45e62850d1b878310c2d17f04347070f4378
lian842 <lian874@yonsei.ac.kr>
2024-12-04 14:06:07 +0900

    made battle page ~ latest skill, changed Struct Pokemon to print out the currency of the battle (latest skill, effectiveness)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
@@ -17,12 +18,13 @@ int typeeffective [6][6] ={ // when super effective :1, effective :0, not very e
 
 
 
-
 struct Pokemon {
     string name;
     string type;
+    string latestskill;
+    int effectiveness;
     int HP;
-    vector <Pokestat> skill;
+    vector <struct Pokestat> skills;
 };
 
 
@@ -33,37 +35,61 @@ struct Pokestat {   // make struct to set the skillname, skilltype, damage, maxt
     int maxtry;
 };
 
-
-vector<Pokestat> Pikachu= { // setting of Pikachu
+Pokemon Pikachu = {"Pikachu", "Electric", "-", 2, 35,   // Pikachu stat
+    { 
     {"Takcle", "Normal", 4, 5},
     {"Grass Knot", "Grass", 8, 5},
     {"Thunderbolt", "Electric", 10, 5},
     {"Megabolt", "Electric", 15, 3}
+    }
 };
-vector<Pokestat> Dratini= { // setting of Dratini
+
+Pokemon Dratini = {"Dratini", "Water", "-", 2, 41,  // Dratini stat
+    {
     {"Wrap", "Normal", 4, 10},
     {"Aqua Tail", "Water", 3, 5},
     {"Water Pulse", "Water", 13, 2},
     {"Hyper Beam", "Normal", 20, 1}
+    }
 };
-vector<Pokestat> Eevee= { // setting of Eevee
+
+Pokemon Eevee = {"Eevee", "Normal", "-", 2, 55,     // Eevee stat
+    {
     {"Takcle", "Normal", 4, 5},
     {"Sand Attack", "Ground", 8, 3},
     {"Bite", "Normal", 12, 3},
-    {"Rain Dance", "Water", 15, 1}
+    {"Rain Dance", "Water", 15, 1}  
+    }
 };
-vector<Pokestat> Charmander= { // setting of Charmander
+
+Pokemon Charmander = {"Charmander", "Fire", "-", 2, 39, // Charmander stat 
+    {
     {"Takcle", "Normal", 4, 5},
     {"Flamehrower", "Fire", 11, 5},
     {"Dig", "Ground", 7, 5},
     {"Heat Wave", "Fire", 14, 5}
+    }
 };
-vector<Pokestat> Palkia= { // setting of Palkia
+
+Pokemon Palkia = {"Palkia", "Water", "-", 2, 90,    // Palkia stat
+    {
     {"Hydro Pump", "Water", 12, 10},
     {"Earth Power", "Ground", 15, 10},
     {"Surf", "Water", 13, 10},
     {"Spatial Rend", "Normal", 30, 10}
+    }
 };
+
+
+void battlePage (Pokemon &pkm1, Pokemon &pkm2, int i) {  
+     // prints out the Battle Page 
+    // gets two Pokemon and one int
+    // int to check who is in the turn
+    // no return, made the function to only print out the Battle Page
+}
+
+
+
 
 
 
@@ -71,4 +97,40 @@ vector<Pokestat> Palkia= { // setting of Palkia
 
 int main(){
     return 0;
+}
+
+
+
+void battlePage (Pokemon &pkm1, Pokemon &pkm2, int i) {    //since 63 == 1 + 60 + 1 + 60 + 1, we use setw to match the condition
+    cout << "+-------------------------------------------------------------+"<< endl;
+    cout << "| 2023149003 OOP Computer Science                             |"<< endl;
+    cout << "+------------------------------+------------------------------+"<< endl;
+    if (i%2 == 1){
+        cout << "|" << setw(30) << left <<  pkm1.name + " (*)" << "|" << setw(30) << left << pkm2.name << "|" << endl; 
+    } else {
+        cout << "|" << setw(30) << left <<  pkm1.name << "|" << setw(30) << left << pkm2.name + " (*)" << "|" << endl; 
+    }
+    cout << "|" << setw(30) << left <<  pkm1.name + " (*)" << "|" << setw(30) << left << pkm2.name << "|" << endl; 
+    cout << "|" << setw(30) << left << "Type: " + pkm1.type << "|" << setw(30) << left << "Type: " + pkm2.type << "|" <<endl;
+    cout << "|" << setw(30) << left << "HP: " + pkm1.HP << "|" << setw(30) << left << "HP: " + pkm2.HP << "|" <<endl;
+    cout << "+------------------------------+------------------------------+"<< endl;
+    cout << "|" << setw(30) << left << "Latest skill: " + pkm1.latestskill << "|" << setw(30) << left << "Latest Skill: " + pkm2.latestskill << "|" <<endl;
+    cout << "|" << setw(30) << left;
+    if (pkm1.effectiveness == 1){
+        cout << " It was super effective";
+    } else if (pkm1.effectiveness == 0){
+        cout << " It was effective";
+    } else if (pkm1.effectiveness == -1) {
+        cout << " It was not very effective";
+    }
+    cout << "|" << setw(30) << left;
+        if (pkm2.effectiveness == 1){
+        cout << " It was super effective";
+    } else if (pkm2.effectiveness == 0){
+        cout << " It was effective";
+    } else if (pkm2.effectiveness == -1) {
+        cout << " It was not very effective";
+    }
+    cout << "|" << endl;
+    cout << "+------------------------------+------------------------------+"<< endl;
 }
