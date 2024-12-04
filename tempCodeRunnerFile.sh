adf54945cec0a97f17877ec66eda054ef01f15fb
lian842 <lian874@yonsei.ac.kr>
2024-12-04 20:12:50 +0900

    Update run.sh and add tempCodeRunnerFile.sh

--- tempCodeRunnerFile.sh
+++ tempCodeRunnerFile.sh
@@ -4,7 +4,7 @@
 mkdir -p build
 
 # Compile the program with C++17, output to build/main
-g++ -std=c++17 /Users/_lian_/Desktop/oop/src/main.cpp -o build/main
+g++ -std=c++17 /Users/_lian_/Desktop/oop/main.cpp -o build/main
 
 # Remove stray executables from src directory (cleanup)
 rm -f src/main
@@ -15,4 +15,3 @@ if [ -f "$1" ]; then
 else
     echo "Usage: ./run.sh input_file output_file"
 fi
-
